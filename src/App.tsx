1c7ee6b814979219870c5622ed920b8053659444
Cherry <darienreese@gmail.com>
2021-04-05 13:24:59 -0400

    message listener exists now cool

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Comment } from './classes/Comment'
 import './App.css';
+import MessageListener from './classes/MessageListener'
+import ContentAPI from './classes/ContentAPI';
 
 export default function App() {
-  const [comment, setComment] = useState<Array<Comment> | undefined>(undefined);
+  const chatLoaded = useRef<boolean>(false);
+  const listener: React.MutableRefObject<MessageListener | undefined> = useRef();
+  const [comment, setComment] = useState<Array<Comment>>([]);
+  const [roomNumber,] = useState<number>(384);
 
   // load a comment from the server
   useEffect(() => {
     // Comment.send('hello, world', {}, 384, ContentAPI.TEST_AUTHTOKEN)
     //   .then(x => setComment(x));
-    Comment.addMessageEvent(
-      "(function(message){message.textContent=message.createUser.username + 's message'})"
-    )
-    Comment.getWithLimit(5, 384)
-      .then((x: Array<Comment>) => setComment(x));
-  }, []);
+    // Comment.addMessageEvent(
+    //   "(function(message){message.textContent=message.createUser.username + 's message'})"
+    // );
+    if (!chatLoaded.current) {
+      chatLoaded.current = true;
+      listener.current = new MessageListener(ContentAPI.TEST_AUTHTOKEN);
+      Comment.getWithLimit(5, roomNumber)
+        .then((x: Array<Comment>) => {
+          setComment(x);
+          listener.current?.addCallback('chatpane-384', (c: Comment) => {
+            setComment(comment => comment.concat([c]))
+          });
+        });
+    }
+  }, [roomNumber]);
 
   return (
     <div>
-      {comment !== undefined && comment.reverse().map((x: Comment) => 
+      {comment.map((x: Comment) => 
         <div key={x.id}>
           <img 
             src={x.createUser.getAvatarLink(64)} 
--- src/classes/MessageListener.ts
+++ src/classes/MessageListener.ts
@@ -1,40 +1,73 @@
 import ContentAPI from "./ContentAPI";
-import { Comment } from './Comment'
+import { Comment, CommentData } from './Comment'
+
+interface ListenerSettings {
+    lastId: number;
+    chains: Array<string>;
+}
+
+interface ListenerResponse {
+    chains: any;
+    lastId: number;
+}
 
 // sends new comments as "newComment" events
 export default class MessageListener {
     private authtoken: string;
     private lastId: number = -1;
-    private timeout: number = -1;
     private running: boolean = false;
+    private callbacks: Array<Function> = [];
 
     constructor(authtoken: string) {
         this.authtoken = authtoken;
+        this.runForever();
     }
 
     private call() {
         if (this.running) {
-            // make an initial request if a lastId doesn't exist
-            if (this.lastId === -1) {
-                Comment.getWithLimit(1)
-                    .then((comment: Array<Comment>) => {
-                        this.lastId = comment[0].id;
-                        this.running = true;
-                        this.timeout = setTimeout(this.call);
-                    })
-            // otherwise, let's make a call to the chainer to grab comments
-            } else {
-
-            }
+            // let's make a call to the chainer to grab comments
+            let settings: ListenerSettings = {
+                lastId: this.lastId,
+                chains: ['comment.0id', 'user.1createUserId',
+                            'content.1parentId']
+            };
+            let url = `${ContentAPI.API_LINK}Read/listen?actions=` +
+                        JSON.stringify(settings);
+            fetch(url, {
+                headers: {
+                    'Authorization': 'Bearer ' + this.authtoken
+                }
+            })
+                .then(response => response.json())
+                .then((response: ListenerResponse) => {
+                    this.lastId = response.lastId;
+                    const comments: Array<Comment> = response.chains.comment.map((c: CommentData) => 
+                        new Comment(c, response.chains.user)
+                    );
+                    this.callbacks.forEach((callback: Function) => {
+                        comments.forEach((comment: Comment) => {callback(comment)})
+                    });
+                    this.call();
+                })
+                .catch((error: Error) => {console.error(error)})
         }   
     }
 
     runForever() {
         this.running = true;
-        this.timeout = setTimeout(this.call)
+        Comment.getWithLimit(1)
+            .then((comment: Array<Comment>) => {
+                this.lastId = comment[0].id;
+                this.running = true;
+                this.call();
+            })
     }
 
     endEventLoop() {
         this.running = false;
+    }
+
+    addCallback(id: string, callback: Function) {
+        this.callbacks.push(callback);
     }
 }
